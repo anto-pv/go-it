eed938c2e8fda786d662a989a96606427da68c5a
anto-pv <antopv833@gmail.com>
2021-02-05 09:00:27 +0530

    routes

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                Home
+                About Us
+            </nav>
+        </div>
+    );
+};
+export default Header;
