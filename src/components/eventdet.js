940e6763117ad932dd555149d91226ff114af724
anto-pv <antopv833@gmail.com>
2021-04-10 16:31:33 +0530

    test

--- src/components/eventdet.js
+++ src/components/eventdet.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import imgs from './svg/mdm.png';
+import img2 from './svg/Ai.png';
+import img3 from './svg/bim2.jpg';
 
 const Eventd = () => {
     return (
@@ -11,11 +13,11 @@ const Eventd = () => {
                     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
                 <div class="carousel-inner">
-                    <div class="carousel-item active" data-bs-interval="1000">
+                    <div class="carousel-item active" data-bs-interval="5000">
                         <div className="slides">
                             <p className="heading">Modern Manufacturing &amp;Inventive Design Internship</p>
                             <div className="rows">
-                                <div className="text"><p className="qot">"Everything is designed. Few things are designed well."— Brian Reed</p>
+                                <div className="text"><p className="qot"><i>"Everything is designed. Few things are designed well."</i>— Brian Reed</p>
                                 <ul>
                                     <li>Online Virtual Internship</li>
                                     <li>Training by Industry Experts</li>
@@ -28,36 +30,44 @@ const Eventd = () => {
                                 <img src={imgs} alt ="mdm" />
                             </div>
                         </div>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
                     </div>
+                    <div class="carousel-item" data-bs-interval="5000">
+                        <div className="slides">
+                            <p className="heading"> Construction Technology: BIM</p>
+                            <div className="rows">
+                                <div className="text"><p className="qot"><i> “We shape our buildings; thereafter, they shape us.”</i> – Winston Churchill</p>
+                                <ul>
+                                    <li>Online Virtual Internship</li>
+                                    <li>Training by Industry Experts</li>
+                                    <li>Project Based learning</li>
+                                    <li>Flexible Timing</li>
+                                    <li>3 CAD Softwares in 10 Days</li>
+                                </ul>
+                                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdHXwn-VPj6_dWlFl40KXVunNE7CIVkeTeJHS3dATuWPGxByw/viewform" className="btn">Register Now</a>
+                                </div>
+                                <img src={img3} alt ="img3" />
+                            </div>
+                        </div>
                     </div>
-                    <div class="carousel-item" data-bs-interval="1000">
+                    <div class="carousel-item" data-bs-interval="5000">
                     <div className="slides">
-                        
-                    </div>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the second slide.</p>
-                    </div>
-                    </div>
-                    <div class="carousel-item" data-bs-interval="1000">
-                    <img src="..." class="d-block w-100" alt="..."/>
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the third slide.</p>
+                        <p className="heading">AI and Modern Computer Science</p>
+                        <div className="rows">
+                            <div className="text"><p className="qot"><i>“As a technologist, I see how AI and the fourth industrial revolution will impact every aspect of people’s lives.”</i> – Fei-Fei Li</p>
+                            <ul>
+                                <li>Online Virtual Internship</li>
+                                <li>Training by Industry Experts</li>
+                                <li>Project Based learning</li>
+                                <li>Flexible Timing</li>
+                                <li>3 CAD Softwares in 10 Days</li>
+                            </ul>
+                            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdHXwn-VPj6_dWlFl40KXVunNE7CIVkeTeJHS3dATuWPGxByw/viewform" className="btn">Register Now</a>
+                            </div>
+                        <img src={img2} alt ="img2" />
+                         </div>
                     </div>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
             </div>
         </div>
     );
